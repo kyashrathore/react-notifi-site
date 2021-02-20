2890ea89e3f03c81a3934bdc4edd11d628cbbce4
yashvardhan <yashvardhan@recko.io>
2021-02-20 18:23:19 +0530

    fix lint errors

--- packages/react-app/src/index.js
+++ packages/react-app/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Notifi, EnqueNotifi } from 'react-notifi';
 import DefaultSnack from './wrapper';
